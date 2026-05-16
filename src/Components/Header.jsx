a4e20aefb12c60d3b68705d70a5c576ed9f9ad92
Pradnya Patil <ppradnya391@gmail.com>
2026-05-16 22:53:59 +0530

    again changed logo path

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <header>
-        <img src="./game-logo.png" />
+        <img src="/game-logo.png" />
         <h1>Play Tic-Tac-Toe Game for Fun</h1>
       </header>
     </>
