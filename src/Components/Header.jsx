cd57ade801e8f47002e5444086cd6a0cfbdc76b0
Pradnya Patil <ppradnya391@gmail.com>
2026-05-16 22:23:54 +0530

    added header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <img src="/game-logo.png" />
+        <h1>Play Tic-Tac-Toe Game for Fun</h1>
+      </header>
+    </>
+  );
+}
+export default Header;
